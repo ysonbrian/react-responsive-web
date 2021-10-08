6f79f10c0fd5b7b029f592cd9adce191a9def121
ysonbrian <znlsakxls@gmail.com>
2021-10-08 22:27:05 +0900

    Project Setting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const App = () => {
+  return <Container>Hello World!</Container>;
+};
+
+export default App;
