be05ad0fe9abe207382c397a9a364fbec6d51e7e
ysonbrian <znlsakxls@gmail.com>
2021-10-10 20:01:46 +0900

    Product Introduction Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import styled from 'styled-components';
+import Intro from './components/Intro';
 
 const Container = styled.div`
   height: 100vh;
+  overflow: hidden;
 `;
 
 const App = () => {
   return (
     <Container>
       <Navbar />
+      <Intro />
     </Container>
   );
 };
